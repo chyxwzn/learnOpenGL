b4324fa61481d610f932504143dd8eb9590e1bd9
Shawn Ding <chyxwzn@gmail.com>
2017-03-11 00:16:49 +0800

    Add first demo

---
+++ Demos/ClearColor/sources/main.cpp
@@ -0,0 +1,60 @@
+// Local Headers
+#include "common.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char * argv[]) {
+    int w, h;
+
+    // Load GLFW and Create a Window
+    glfwInit();
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+    // auto mWindow = glfwCreateWindow(mode->width, mode->height, "OpenGL", nullptr, nullptr);
+
+    // Check for Valid Context
+    if (mWindow == nullptr) {
+        fprintf(stderr, "Failed to Create OpenGL Context");
+        return EXIT_FAILURE;
+    }
+
+    // Create Context and Load OpenGL Functions
+    glfwMakeContextCurrent(mWindow);
+    gladLoadGL();
+    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    glfwGetFramebufferSize(mWindow, &w, &h);
+    printf("framebuffer size: %d x %d\n", w, h);
+
+    // Background Fill Color
+    glClearColor(1.0f, 0, 0, 1.0f);
+    // glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    // Rendering Loop
+    while (glfwWindowShouldClose(mWindow) == false) {
+        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(mWindow, true);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Flip Buffers and Draw
+        glfwSwapBuffers(mWindow);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
